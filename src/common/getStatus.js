3e8c8d0692b8e2e5a5b0b7510bb2c4c4223ec62a
Rodrigo Alves <rodrigo.rufino95@gmail.com>
2021-07-08 15:47:45 -0300

    Adicionado get status para pegar status pelo id

---
+++ src/common/getStatus.js
@@ -0,0 +1,24 @@
+const createStatus = (name, icon, color) => {
+  return { name, icon, color };
+};
+
+const getStatus = (id) => {
+  switch (id) {
+    case 0:
+      return createStatus('Todos', 'table', '#D190DD');
+    case 1:
+      return createStatus('Em execução', 'sent', '#C1CA4F');
+    case 2:
+      return createStatus('Ativa', 'activated', '#35C667');
+    case 3:
+      return createStatus('Configurando', 'configuration', '#35C667');
+    case 4:
+      return createStatus('Ociosa', 'idle', '#EBBD3E');
+    case 5:
+      return createStatus('Concluida', 'completed', '#9FABD5');
+    default:
+      console.error('[ERROR]: Icon not found.');
+  }
+};
+
+export default getStatus;
